28a9494076c4a6555291e85700d699e68545c52e
Mohamed ZITOUNI <zitouni.mohamed.email@gmail.com>
2022-04-29 00:28:22 +0200

    nodejs basics part3

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 const http = require('http');
+const { requestListener } = require('./routes'); 
 
-// Core Modules: http, https, os, path, fs
-// Local Modules: 
-// Third Pard Modules: rxjs
-
-http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Welcome Page</title></head><body>');
-    res.write('<h2>Hello from the server!');
-    res.write('</body></html>');
-    res.end();
-
-}).listen(3000);
+http.createServer(requestListener).listen(3000, () => { console.log('Server is running on port 3000 ...')});
