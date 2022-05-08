431e421a7ba4fcb94cb2c56ce4adb9e780955257
Mohamed ZITOUNI <zitouni.mohamed.email@gmail.com>
2022-05-08 04:10:54 +0200

    express js part 2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
-const { welcomeRoutes } = require('./welcome.routes'); 
-const { connectionRoutes } = require('./connection.routes'); 
-const { loginRoutes } = require('./login.routes'); 
-const { productRoutes } = require('./product.routes'); 
+const { welcomeRoutes } = require('./routes/welcome'); 
+const { connectionRoutes } = require('./routes/connection'); 
+const { loginRoutes } = require('./routes/login'); 
+const { productRoutes } = require('./routes/product'); 
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/connection', connectionRoutes);
 app.use('/login', loginRoutes);
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,27 +1,33 @@
-const productRoutes = (req, res) => {
-    if(req.method === 'POST'){
-        res.statusCode = 201;
-        const {title, price, amount, postAction} = req.body;
-        if(!products.some(product => product.title === title)){
-            products.push({title, price, amount});
-        } else if(postAction === 'update'){
-            products.forEach(product => {
-                if(product.title === title){
-                    product.price = price;
-                    product.amount = amount;
-                }
-            });
-        }
-        res.end(template());
-    } else if(req.method === 'DELETE'){
-        const title = req.url.split('title=')[1];
-        products = products.filter(product => product.title !== title);
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('success');
-    }else {
-        res.end(template());
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    res.statusCode = 201;
+    const {title, price, amount, postAction} = req.body;
+    if(!products.some(product => product.title === title)){
+        products.push({title, price, amount});
+    } else if(postAction === 'update'){
+        products.forEach(product => {
+            if(product.title === title){
+                product.price = price;
+                product.amount = amount;
+            }
+        });
     }
-};
+    res.end(template());
+});
+
+router.delete('/', (req, res) => {
+    const title = req.url.split('title=')[1];
+    products = products.filter(product => product.title !== title);
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('success');
+});
+
+router.get('/', (req, res) => {
+    res.end(template());
+});
 
 let products = [{title: "t1", price: '1', amount: '42'}, {title: "t2", price: '34', amount: '2'}, {title: "t3", price: '53', amount: '6'}];
 
@@ -134,4 +140,4 @@ const template = () => `
 </html>
 `;
 
-exports.productRoutes = productRoutes;
+exports.productRoutes = router;
