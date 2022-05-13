1c1024095faa01d326f67555b0bea061821bbe46
Mohamed ZITOUNI <zitouni.mohamed.email@gmail.com>
2022-05-13 23:12:00 +0200

    work with MVC

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,42 +1,12 @@
 const express = require('express');
+const productsController = require('../controllers/product');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    res.statusCode = 201;
-    const {title, price, amount, postAction} = req.body;
-    if(!products.some(product => product.title === title)){
-        products.push({title, price, amount});
-    } else if(postAction === 'update'){
-        products.forEach(product => {
-            if(product.title === title){
-                product.price = price;
-                product.amount = amount;
-            }
-        });
-    }
-    res.render('product', {
-        products: products,
-        pageTitle: 'Products Page',
-        page: 'product'
-    });
-});
+router.post('/', productsController.save);
 
-router.delete('/', (req, res) => {
-    const title = req.url.split('title=')[1];
-    products = products.filter(product => product.title !== title);
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('success');
-});
+router.delete('/',  productsController.delete);
 
-router.get('/', (req, res) => {
-    res.render('product', {
-        products: products,
-        pageTitle: 'Products Page',
-        page: 'product'
-    });
-});
-
-let products = [{title: "t1", price: '1', amount: '42'}, {title: "t2", price: '34', amount: '2'}, {title: "t3", price: '53', amount: '6'}];
+router.get('/',  productsController.getProducts);
 
 exports.productRoutes = router;
