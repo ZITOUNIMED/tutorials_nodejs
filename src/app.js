edae2a02890d9245af57948730f9d83138164d16
Mohamed ZITOUNI <zitouni.mohamed.email@gmail.com>
2022-05-13 11:38:58 +0200

    Ejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { engine } = require('express-handlebars');
 
 const { welcomeRoutes } = require('./routes/welcome'); 
 const { connectionRoutes } = require('./routes/connection'); 
@@ -10,8 +9,7 @@ const { productRoutes } = require('./routes/product');
 
 const app = express();
 
-app.engine('hbs', engine({defaultLayout: 'main', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
 app.use(express.static(path.join(__dirname, 'public')));
